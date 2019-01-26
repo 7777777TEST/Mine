f08598e0b0a6b4961bc2f49f66f74b601aad98b2
7777777TEST <45910952+7777777TEST@users.noreply.github.com>
2019-01-26 18:20:29 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,41 @@
-var CACHE_NAME = 'mine-caches';
-var url=['/Mine/'];
-self.addEventListener('install',function(event){
-    event.waitUntil(caches.open(CACHE_NAME).then(function(cache){return cache.addAll(url);}));
+var CACHE_NAME = 'minesweeper-caches';
+var appShellFiles = [
+  '/Mine/',
+  '/Mine/index.html',
+  '/Mine/images/app.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(appShellFiles);
+    })
+  );
 });
-self.addEventListener('fetch',function(event){
-    event.respondWith(caches.match(event.request).then(function(response){return response?response:fetch(event.request);}));
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME !== cacheName &&  cacheName.startsWith("minesweeper-caches")) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
